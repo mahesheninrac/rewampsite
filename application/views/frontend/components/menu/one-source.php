b042c9a8ec705bc0cf46b1f53fb989482b8d850c
mahesheninrac <mahesh.solanke@eninrac.com>
2025-09-26 15:20:31 +0530

    updated the one source

--- application/views/frontend/components/menu/one-source.php
+++ application/views/frontend/components/menu/one-source.php
@@ -3,33 +3,24 @@
         <div class="content">
             <div class="">
                 <div class="row">
-<<<<<<< HEAD
-                    <div class="col-md-8 p-0" style="height: 87vh;">
-=======
                     <div class="col-md-8 p-0 vh-78">
->>>>>>> origin/layout
                         <div class="mega-menu position-relative p-4">
                             <ul class="d-flex flex-column gap-2">
                                 <li class="row service-item">
                                     <div class="col-md-5">
-                                        <div class="d-flex w-100 align-items-center justify-content-between gap-2 service-link">
-<<<<<<< HEAD
-                                            <span class="fw-bold">Budget</span>
-=======
+                                        <div
+                                            class="d-flex w-100 align-items-center justify-content-between gap-2 service-link">
                                             <span class="fw-500">Budget</span>
->>>>>>> origin/layout
                                             <i class="icofont-rounded-right" style="display: inline-block;"></i>
                                         </div>
                                     </div>
                                     <div class="col-md-7">
                                         <div class="servicemenu-wrap active">
-                                            <div style="width: calc(100% - 25px);border-bottom: 1px solid #d8d7d7;" class="d-flex justify-content-between align-items-center gap-5 mb-3">
-<<<<<<< HEAD
-                                                <h3 class="fs-16 fw-bold">India Budget</h3>
-=======
+                                            <div style="width: calc(100% - 25px);border-bottom: 1px solid #d8d7d7;"
+                                                class="d-flex justify-content-between align-items-center gap-5 mb-3">
                                                 <h3 class="fs-16 fw-600">India Budget</h3>
->>>>>>> origin/layout
-                                                <a href="https://eninrac.com/budget" class="position-relative fs-14" style="left: -15px;">View 
+                                                <a href="https://eninrac.com/budget" class="position-relative fs-14"
+                                                    style="left: -15px;">View
                                                     <span class="ms-2 cta -arrow"></span>
                                                 </a>
                                             </div>
@@ -70,39 +61,31 @@
                                 </li>
                                 <li class="row service-item">
                                     <div class="col-md-5">
-<<<<<<< HEAD
-                                        <a href="https://eninrac.com/knowledge-grid" class="d-flex w-100 align-items-center justify-content-between gap-2 service-link-active fw-bold">Knowledge Grid</a>
-=======
-                                        <a href="https://eninrac.com/knowledge-grid" class="d-flex w-100 align-items-center justify-content-between gap-2 service-link-active fw-500">Knowledge Grid</a>
->>>>>>> origin/layout
+                                        <a href="https://eninrac.com/knowledge-grid"
+                                            class="d-flex w-100 align-items-center justify-content-between gap-2 service-link-active fw-500">Knowledge
+                                            Grid</a>
                                     </div>
                                 </li>
                                 <li class="row service-item">
                                     <div class="col-md-5">
-<<<<<<< HEAD
-                                        <a href="https://survey.eninrac.com/" class="d-flex w-100 align-items-center justify-content-between gap-2 service-link-active fw-bold">Eninrac Survey</a>
-=======
-                                        <a href="https://survey.eninrac.com/" class="d-flex w-100 align-items-center justify-content-between gap-2 service-link-active fw-500">Eninrac Survey</a>
->>>>>>> origin/layout
+                                        <a href="https://survey.eninrac.com/"
+                                            class="d-flex w-100 align-items-center justify-content-between gap-2 service-link-active fw-500">Eninrac
+                                            Survey</a>
                                     </div>
                                 </li>
                             </ul>
                         </div>
                     </div>
-<<<<<<< HEAD
-                    <div class="col-md-4" style="background: blue;height: 78vh;">
-                        <div class="d-flex align-items-center justify-content-center service-menu" style="position: relative;height: 100%;">
-                            <div style="position: absolute; top: 15px; left: 15px;z-index: 1;">
-=======
                     <div class="col-md-4 nav-menu-sidebar vh-78" style="background: blue;">
-                        <div class="d-flex align-items-center justify-content-center service-menu" style="position: relative;height: 100%;">
+                        <div class="d-flex align-items-center justify-content-center service-menu"
+                            style="position: relative;height: 100%;">
                             <div class="content-wrapper">
->>>>>>> origin/layout
                                 <h3 class="text-white fs-20 fw-500 mb-2">
                                     Connect with us
                                 </h3>
                                 <p class="d-block fs-14 text-white">
-                                    Eninrac consulting has been consistently combining high aspirations and unique perspectives to enable client's with "difference margin"
+                                    Eninrac consulting has been consistently combining high aspirations and unique
+                                    perspectives to enable client's with "difference margin"
                                 </p>
                                 <ul>
                                     <li>
@@ -118,19 +101,12 @@
                                     </li>
                                 </ul>
                             </div>
-<<<<<<< HEAD
-                            <a href="https://eninrac.com/contact">
-                                <span style="position: absolute;z-index: 1;width: 60px;height: 60px;margin: auto;left: 10px;bottom: 10px;justify-content: center;align-items: center;color: #00f;cursor: pointer;background: #f8f9fa;display: flex;border-radius: 50%;font-size: 28px;text-align: center;transform: rotate(45deg);" class="icofont-long-arrow-up"></span>
-                            </a>
-                            <img src="<?= base_url() ?>assets/images/contact-us.png" alt="" style="width: calc(100% - 145px);position: absolute;right: 0;bottom: 0;">
-=======
-                            <a class="menu-side-active-link"  href="https://eninrac.com/contact">
+                            <a class="menu-side-active-link" href="https://eninrac.com/contact">
                                 <span class="icofont-long-arrow-up"></span>
                             </a>
                             <div class="menu-side-image-placeholder">
                                 <img src="<?= base_url() ?>assets/images/thinking.png" alt="">
                             </div>
->>>>>>> origin/layout
                         </div>
                     </div>
                 </div>
